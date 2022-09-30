f06a33f60ecfff8f3c89a1036f0b770d00957646
Dominic Paulazzo <dompaulazzo@gmail.com>
2022-09-30 12:55:17 -0700

    Adding more to the JS page, getting the pop-up windows to work and log the true/false user inputs.

--- script.js
+++ script.js
@@ -9,7 +9,48 @@ function generatePassword() {
  
   if(isNaN(passwordLength)) {
     window.alert("That is not a number. Please input the numbers of characters you want in your password length.")
+    return
   }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be at least 8 characters long but no more that 128.")
+    return
+  }
+
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+  var userLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userSymbols = window.confirm("Would you like to include special characters in your password?")
+
+  var numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", ">", "<", "?"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = []
+  
+  var optionsSpecs = []
+  
+  for (var i = 0; i < lowercaseList.length; i++) {
+    uppercaseList[i] = lowercaseList[i].toUpperCase()
+  }
+  
+  if (userWantsNumbers === true) {
+    optionsSpecs.push(numberList)
+  }
+
+  if (userUppercase === true) {
+    optionsSpecs.push(uppercaseList)
+  }
+
+  if (userLowercase === true) {
+    optionsSpecs.push(lowercaseList)
+  }
+
+  if (userSymbols === true) {
+    optionsSpecs.push(symbolList)
+  }
+
+  console.log(optionsSpecs)
+
 }
 
 // Write password to the #password input
